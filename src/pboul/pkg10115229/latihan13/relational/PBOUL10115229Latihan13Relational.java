17c662dc8d71fadcba7b32b9fccbea623bb87cde
fakhri ramdani <muhammadlukman09@gmail.com>
2019-09-26 10:21:59 +0700

    project ketigabelas

---
+++ src/pboul/pkg10115229/latihan13/relational/PBOUL10115229Latihan13Relational.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboul.pkg10115229.latihan13.relational;
+
+/**
+ *
+ * @author Lukman
+ * NIM          : 10115229
+ * NAMA         : MUHAMMAD LUKMAN HAKIM
+ * HARITANGGAL  : SABTU, 21 SEPTEMBER 2019
+ * LOG
+ */
+public class PBOUL10115229Latihan13Relational {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int bil1 = 10;
+        int bil2 = 20;
+        
+        System.out.println("a == b = " + (bil1 == bil2));
+        System.out.println("s != b = " + (bil1 != bil2));
+        System.out.println("a > b = " + (bil1 > bil2));
+        System.out.println("a < b = " + (bil1 < bil2));
+        System.out.println("b >= a = " + (bil2 >= bil1));
+        System.out.println("b <= a = " + (bil2 <= bil1));
+    }
+    
+}
